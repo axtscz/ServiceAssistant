10c3d17caaee1d8372e18bc6a44f976cf11c2929
axtscz <axtscz@gmail.com>
2014-08-22 14:07:23 -0500

    3rd commit

--- app/build/generated/source/r/debug/com/collusion/serviceassistant/R.java
+++ app/build/generated/source/r/debug/com/collusion/serviceassistant/R.java
@@ -38,10 +38,11 @@ public final class R {
         public static final int ic_pages=0x7f020008;
         public static final int ic_people=0x7f020009;
         public static final int ic_photos=0x7f02000a;
-        public static final int ic_whats_hot=0x7f02000b;
-        public static final int list_item_bg_normal=0x7f02000c;
-        public static final int list_item_bg_pressed=0x7f02000d;
-        public static final int list_selector=0x7f02000e;
+        public static final int ic_tractcounter64=0x7f02000b;
+        public static final int ic_whats_hot=0x7f02000c;
+        public static final int list_item_bg_normal=0x7f02000d;
+        public static final int list_item_bg_pressed=0x7f02000e;
+        public static final int list_selector=0x7f02000f;
     }
     public static final class id {
         public static final int GoalBar=0x7f0a0011;
